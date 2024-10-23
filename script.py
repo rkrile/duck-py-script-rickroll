5a2e5e8703c4a11f3e54ac0f5f748e5a23d9fa8a
rkrile <rkriley@ucmo.edu>
2024-10-23 16:55:36 -0500

    script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ import time
 
 while True:
     # Move the mouse cursor randomly
-    x_offset = random.randint(-20, 20)
-    y_offset = random.randint(-20, 20)
+    x_offset = random.randint(-200, 200)
+    y_offset = random.randint(-200, 200)
     pyautogui.moveRel(x_offset, y_offset, duration=0.25)
-    time.sleep(20)  # 20 second delay
+    time.sleep(40)  # 40 second delay, testing limits
